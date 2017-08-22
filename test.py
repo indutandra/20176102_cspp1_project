ca17e9fa49b4804413c10d5ff1948bf81b7c9304
indutandra <indutandra@gmail.com>
2017-08-22 17:23:02 +0530

    underscore

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import math
-file,file1=open("one.txt","r"),open("two.txt","r")
+file,file1=open("two.txt","r"),open("one.txt","r")
 f1,f2=file.read().lower(),file1.read().lower()
 def ch(l):
 	a=''
 	for c in range(len(l)):
 		if ord(l[c])>=97 and ord(l[c])<=122:
 			a+=l[c]
-		elif ord(l[c])==32:
+		elif ord(l[c])==95 or ord(l[c])==32:
+			a+=l[c]
+		elif ord(l[c])>=48 and ord(l[c])<=57:
 			a+=l[c]
 		else:
 			c+=1
@@ -36,9 +38,7 @@ def dot(e,f):
 				sum=sum+mul
 	return sum
 g=(ch(f1).split(" "))
-print("g=",g)
 h=(ch(f2).split(" "))
-print("h=",h)
 e,f=dic(g),dic(h)
 t=norm(e,f)
 print("pliagarism of two files is",(dot(e,f)/t)*100,"%")
